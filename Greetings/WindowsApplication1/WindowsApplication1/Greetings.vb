b45dcc9eb1201416fabb31247b54c974fe5f9d6b
rphayes2102 <60491812+rphayes2102@users.noreply.github.com>
2020-02-06 20:29:50 -0500

    print to listbox

--- Greetings/WindowsApplication1/WindowsApplication1/Greetings.vb
+++ Greetings/WindowsApplication1/WindowsApplication1/Greetings.vb
@@ -1,20 +1,32 @@
 ﻿Public Class Greetings
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
-
-        'Take value from name textbox and print in messagebox
-        'MessageBox.Show(txtName.Text)
-
-
-        'Take vaule from name tetxbox and print to txtOut
-        txtOut.Text = "Good Evening " & txtName.Text & ControlChars.CrLf
-        txtOut.Text = txtOut.Text & " How are You?"
+        'Print Greetings to listbox
+        lstOut.Items.Add("Good Evening " & txtName.Text)
+        lstOut.Items.Add("How are you? ")
 
 
         'Clear txtName value
         txtName.Clear()
+
     End Sub
 
-    Private Sub txtOut_TextChanged(sender As Object, e As EventArgs) Handles txtOut.TextChanged
+    Private Sub txtOut_TextChanged(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Greetings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear txtName value
+        txtName.Clear()
+        lstOut.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'when user exits the program
+        Beep()
+        Application.Exit()
     End Sub
 End Class
