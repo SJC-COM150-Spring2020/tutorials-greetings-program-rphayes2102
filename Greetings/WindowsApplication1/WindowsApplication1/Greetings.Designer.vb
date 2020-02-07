9dcaa909b82284b89048b2f3ddd75b723d1238a1
rphayes2102 <60491812+rphayes2102@users.noreply.github.com>
2020-02-06 19:52:57 -0500

    Printed greetings to text box
    Printed greetings from txtName to txtOut

--- Greetings/WindowsApplication1/WindowsApplication1/Greetings.Designer.vb
+++ Greetings/WindowsApplication1/WindowsApplication1/Greetings.Designer.vb
@@ -25,6 +25,8 @@ Partial Class Greetings
         Me.btnClick = New System.Windows.Forms.Button()
         Me.txtName = New System.Windows.Forms.TextBox()
         Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtOut = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'btnClick
@@ -32,7 +34,7 @@ Partial Class Greetings
         Me.btnClick.BackColor = System.Drawing.Color.Tomato
         Me.btnClick.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnClick.ForeColor = System.Drawing.Color.DarkGreen
-        Me.btnClick.Location = New System.Drawing.Point(195, 237)
+        Me.btnClick.Location = New System.Drawing.Point(195, 211)
         Me.btnClick.Name = "btnClick"
         Me.btnClick.Size = New System.Drawing.Size(162, 50)
         Me.btnClick.TabIndex = 0
@@ -55,18 +57,43 @@ Partial Class Greetings
         Me.lbl1.BackColor = System.Drawing.Color.White
         Me.lbl1.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl1.ForeColor = System.Drawing.Color.DarkGreen
-        Me.lbl1.Location = New System.Drawing.Point(191, 36)
+        Me.lbl1.Location = New System.Drawing.Point(180, 37)
         Me.lbl1.Name = "lbl1"
         Me.lbl1.Size = New System.Drawing.Size(196, 19)
         Me.lbl1.TabIndex = 2
         Me.lbl1.Text = "Greetings Program"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.White
+        Me.lblName.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.ForeColor = System.Drawing.Color.DarkGreen
+        Me.lblName.Location = New System.Drawing.Point(191, 102)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(185, 19)
+        Me.lblName.TabIndex = 3
+        Me.lblName.Text = "Enter Your Name "
+        '
+        'txtOut
+        '
+        Me.txtOut.BackColor = System.Drawing.Color.Tomato
+        Me.txtOut.Font = New System.Drawing.Font("MS Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOut.ForeColor = System.Drawing.Color.DarkGreen
+        Me.txtOut.Location = New System.Drawing.Point(195, 294)
+        Me.txtOut.Multiline = True
+        Me.txtOut.Name = "txtOut"
+        Me.txtOut.Size = New System.Drawing.Size(238, 56)
+        Me.txtOut.TabIndex = 4
         '
         'Greetings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Window
-        Me.ClientSize = New System.Drawing.Size(529, 356)
+        Me.ClientSize = New System.Drawing.Size(615, 411)
+        Me.Controls.Add(Me.txtOut)
+        Me.Controls.Add(Me.lblName)
         Me.Controls.Add(Me.lbl1)
         Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.btnClick)
@@ -80,4 +107,6 @@ Partial Class Greetings
     Friend WithEvents btnClick As Button
     Friend WithEvents txtName As TextBox
     Friend WithEvents lbl1 As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtOut As TextBox
 End Class
--- Greetings/WindowsApplication1/WindowsApplication1/Greetings.vb
+++ Greetings/WindowsApplication1/WindowsApplication1/Greetings.vb
@@ -2,8 +2,19 @@
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
 
         'Take value from name textbox and print in messagebox
-        MessageBox.Show(txtName.Text)
+        'MessageBox.Show(txtName.Text)
 
+
+        'Take vaule from name tetxbox and print to txtOut
+        txtOut.Text = "Good Evening " & txtName.Text & ControlChars.CrLf
+        txtOut.Text = txtOut.Text & " How are You?"
+
+
+        'Clear txtName value
+        txtName.Clear()
+    End Sub
+
+    Private Sub txtOut_TextChanged(sender As Object, e As EventArgs) Handles txtOut.TextChanged
 
     End Sub
 End Class
